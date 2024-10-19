d00eb0637ab30a39e6b16c42b3f90b78f94c54cc
Jun Anthony <junanthony.business@gmail.com>
2024-10-20 09:45:06 +1300

    Starting Angular

---
+++ src/app/designations/designations.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-designations',
+  standalone: true,
+  imports: [],
+  templateUrl: './designations.component.html',
+  styleUrl: './designations.component.css'
+})
+export class DesignationsComponent {
+
+}
