d2e92ab33973f98ff2d9d2f89d1f5025622a36c5
Jun Anthony <junanthony.business@gmail.com>
2024-10-20 10:07:55 +1300

    Added bootstrap

--- src/app/master/master.component.ts
+++ src/app/master/master.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { RolesComponent } from '../roles/roles.component';
+import { DesignationsComponent } from '../designations/designations.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-master',
   standalone: true,
-  imports: [],
+  imports: [RolesComponent, DesignationsComponent, CommonModule],
   templateUrl: './master.component.html',
   styleUrl: './master.component.css'
 })
 export class MasterComponent {
+  currentComponent: string = '';
 
+  
 }
